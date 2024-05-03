3571b7761d7d6216610313f710098d51b06e42f7
Yilberth Andres <89434567+YilberthAndres@users.noreply.github.com>
2024-05-02 21:01:05 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
+
+
+document.addEventListener("DOMContentLoaded", function() {
+  var year = new Date().getFullYear();
+  document.getElementById("year").innerText = year;
+});
